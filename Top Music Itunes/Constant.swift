40d03abdb9c9121c11aef67e1788acc53d77ac49
delmarz <delmarz.dm22@gmail.com>
2016-10-16 12:36:49 +0800

    Adding Reachability for network

--- Top Music Itunes/Constant.swift
+++ Top Music Itunes/Constant.swift
@@ -10,3 +10,6 @@ import Foundation
 
 typealias JSONDictionary = [String: AnyObject]
 typealias JSONArray = Array<AnyObject>
+let WIFI = "Wifi Available"
+let NOACCESS = "No Internet Access"
+let WWAN = "Cellular Access Available"
