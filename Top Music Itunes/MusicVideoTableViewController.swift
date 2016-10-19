56aa8e2ee631e5bbdad3da6164d88db5bcbf0daa
delmarz <delmarz.dm22@gmail.com>
2016-10-19 23:09:22 +0800

    replace tableView into controller

--- Top Music Itunes/MusicVideoTableViewController.swift
+++ Top Music Itunes/MusicVideoTableViewController.swift
@@ -1,24 +1,23 @@
 //
-//  ViewController.swift
+//  MusicVideoTableViewController.swift
 //  Top Music Itunes
 //
-//  Created by Israel Mayor on 15/10/2016.
+//  Created by Israel Mayor on 19/10/2016.
 //  Copyright © 2016 Team Delmarz. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    @IBOutlet weak var displayLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
+class MusicVideoTableViewController: UITableViewController {
+
+
     var videos = [MusicVideo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.reachabilityStatusChanged), name: Notification.Name(rawValue: "ReachStatusChanged"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MusicVideoTableViewController.reachabilityStatusChanged), name: Notification.Name(rawValue: "ReachStatusChanged"), object: nil)
         reachabilityStatusChanged()
         
         let api = APIManager()
@@ -29,13 +28,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         switch reachabilityStatus {
         case NOACCESS:
             view.backgroundColor = UIColor.red
-            displayLabel.text = "No Internet Access"
+           // displayLabel.text = "No Internet Access"
         case WIFI:
             view.backgroundColor = UIColor.green
-            displayLabel.text = "You are on Wifi Acess!"
+            //displayLabel.text = "You are on Wifi Acess!"
         case WWAN:
             view.backgroundColor = UIColor.yellow
-            displayLabel.text = "You are on Cellular Access!"
+            //displayLabel.text = "You are on Cellular Access!"
         default:
             return
         }
@@ -58,11 +57,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // MARK: - UITableView DataSource
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return videos.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MusicVideoCell", for: indexPath)
         
         let video = videos[indexPath.row]
@@ -74,8 +73,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     
-    
-    // MARK: - UITableView Delegate
-    
-}
 
+}
